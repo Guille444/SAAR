e4b72665f1dc98fa9139e22476d32e6ac1719f22
stanl <20220383@ricaldone.edu.sv>
2024-04-16 00:31:50 -0600

    Comentarios sidebar

--- controllers/admin/citas.js
+++ controllers/admin/citas.js
@@ -1,5 +1,6 @@
+// Cuando se hace clic en el botón, se expande o contrae una barra lateral en la página web. 
 const hamBurger = document.querySelector(".toggle-btn");
 
 hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
+    document.querySelector("#sidebar").classList.toggle("expand");
 });
