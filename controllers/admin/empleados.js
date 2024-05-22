93a9e1ce26113728547b2b741ae4003791410e8f
stanl <20220383@ricaldone.edu.sv>
2024-05-22 10:52:08 -0600

    Plantilla admin

--- controllers/admin/empleados.js
+++ controllers/admin/empleados.js
@@ -1,6 +1,29 @@
+// Constante para completar la ruta de la API.
+const PRODUCTO_API = 'services/admin/producto.php';
+
 // Cuando se hace clic en el botón, se expande o contrae una barra lateral en la página web. 
 const hamBurger = document.querySelector(".toggle-btn");
-
 hamBurger.addEventListener("click", function () {
     document.querySelector("#sidebar").classList.toggle("expand");
+});
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Constante para obtener el número de horas.
+    const HOUR = new Date().getHours();
+    // Se define una variable para guardar un saludo.
+    let greeting = '';
+    // Dependiendo del número de horas transcurridas en el día, se asigna un saludo para el usuario.
+    if (HOUR < 12) {
+        greeting = 'Buenos días';
+    } else if (HOUR < 19) {
+        greeting = 'Buenas tardes';
+    } else if (HOUR <= 23) {
+        greeting = 'Buenas noches';
+    }
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    // MAIN_TITLE.textContent = `${greeting}, bienvenido`;
+    // Llamada a la funciones que generan los gráficos en la página web.
 });
